894cb2d94a54236ebab85b742ee62d5f81f9c786
kebbie228 <anton.konovalchenko@gmail.com>
2022-10-01 18:20:39 +0300

    my first homework

---
+++ src/homework1.java
@@ -0,0 +1,5 @@
+public class homework1 {
+    public static void main(String[] args) {
+        System.out.println("I once had a girl\nOr should I say, she once had me?\nShe showed me her room\nIsn't it good, Norwegian wood?");
+    }
+}
