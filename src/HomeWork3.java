6bfdcce25674dae1669b428589bbfb048ee45d17
kebbie228 <anton.konovalchenko@gmail.com>
2022-10-04 23:51:04 +0300

    my third homework

---
+++ src/HomeWork3.java
@@ -0,0 +1,177 @@
+import java.util.Scanner;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+         /*
+        System.out.println("What day is today");
+        Scanner scanner=new Scanner(System.in);
+        int day=scanner.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Today is Monday");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday");
+                break;
+            case 4:
+                System.out.println("Today is Thursday");
+                break;
+            case 5:
+                System.out.println("Today is  Friday");
+                break;
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+                }
+                */
+        /*
+        System.out.println("What time is it now");
+        Scanner scanner=new Scanner(System.in);
+        int time=scanner.nextInt();
+        if(time>=0 && time<=5 || time==24){
+            System.out.println("Good night");
+        }
+        else if(time>=6 && time<=12){
+            System.out.println("Good morning");
+        }
+        else if(time>=13 && time<=16){
+            System.out.println("Good day");
+        }
+        else if(time>=17 && time<=23){
+            System.out.println("Good evening");
+        }
+        */
+        /*
+        System.out.println("We have got dog,cat,cow,horse,bee,owl,goose,monkey,bird,fish, what sound of animal do you want to hear");
+        Scanner scanner=new Scanner(System.in);
+        String animal=scanner.nextLine();
+        switch(animal){
+            case "dog":
+                System.out.println("wof-wof");
+                break;
+            case "cat":
+                System.out.println("meow");
+                break;
+            case "cow":
+                System.out.println("mo-o");
+                break;
+            case "horse":
+                System.out.println("pr-r");
+                break;
+            case "owl":
+                System.out.println("o-o");
+                break;
+            case "goose":
+                System.out.println("go-o");
+                break;
+            case "monkey":
+                System.out.println("a-a");
+                break;
+            case "bird":
+                System.out.println("pic-pic");
+                break;
+            case "fish":
+                System.out.println("i dont know");
+                break;
+        }
+          */
+        /*
+        System.out.println("Write a coordinate (x,y)");
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        int y=scanner.nextInt();
+        if(x>0 &&y>0){System.out.println("Quadrant of the Cartesian coordinate is I");}
+        if(x>0 && y<0){System.out.println("Quadrant of the Cartesian coordinate is IV");}
+        if(x<0 && y<0){System.out.println("Quadrant of the Cartesian coordinate is III");}
+        if(x<0 && y>0){System.out.println("Quadrant of the Cartesian coordinate is II");}
+        */
+/*
+        System.out.println("Write a birthday someone)");
+        Scanner scanner=new Scanner(System.in);
+        int day=scanner.nextInt();
+        int month=scanner.nextInt();
+        int year=scanner.nextInt();
+        if( (day>=20 && month==1) ||(day<=18 && month==2) ){
+            System.out.println("Your zodiac sign is Aquarius");}
+        if( (day>=19 && month==2) ||(day<=20 && month==3) ){
+            System.out.println("Your zodiac sign is Pisces");}
+        if( (day>=21 && month==3) ||(day<=19 && month==4) ){
+            System.out.println("Your zodiac sign is Aries");}
+        if( (day>=20 && month==4) ||(day<=20 && month==5) ){
+            System.out.println("Your zodiac sign is Taurus");}
+        if( (day>=21 && month==5) ||(day<=21 && month==6) ){
+            System.out.println("Your zodiac sign is Gemini");}
+        if( (day>=22 && month==6) ||(day<=22 && month==7) ){
+            System.out.println("Your zodiac sign is Cancer");}
+        if( (day>=23 && month==7) ||(day<=22 && month==8) ){
+            System.out.println("Your zodiac sign is Leo");}
+        if( (day>=23 && month==8) ||(day<=22 && month==9) ){
+            System.out.println("Your zodiac sign is Virgo");}
+        if( (day>=23 && month==9) ||(day<=23 && month==10) ){
+            System.out.println("Your zodiac sign is Libra");}
+        if( (day>=24 && month==10) ||(day<=21 && month==11) ){
+            System.out.println("Your zodiac sign is Scorpio");}
+        if( (day>=22 && month==11) ||(day<=21 && month==12) ){
+            System.out.println("Your zodiac sign is Sagittarius");}
+        if( (day>=22 && month==12) ||(day<=19 && month==1) ){
+            System.out.println("Your zodiac sign is Capricorn");}
+
+        year=(1972-year)%12;
+
+        if(year<0) {
+            year =Math.abs(year);
+            if (year == 0) System.out.println("Your Chinese year is rat ");
+            if (year == 1) System.out.println("Your Chinese year is ox ");
+            if (year == 2) System.out.println("Your Chinese year is tiger ");
+            if (year == 3) System.out.println("Your Chinese year is rabbit ");
+            if (year == 4) System.out.println("Your Chinese year is dragon ");
+            if (year == 5) System.out.println("Your Chinese year is snake ");
+            if (year == 6) System.out.println("Your Chinese year is horse ");
+            if (year == 7) System.out.println("Your Chinese year is goat ");
+            if (year == 8) System.out.println("Your Chinese year is monkey ");
+            if (year == 9) System.out.println("Your Chinese year is rooster ");
+            if (year == 10) System.out.println("Your Chinese year is dog ");
+            if (year == 11) System.out.println("Your Chinese year is pig ");
+        }
+        else if(year>0){
+            if (year == 0) System.out.println("Your Chinese year is rat ");
+            if (year == 1) System.out.println("Your Chinese year is ox ");
+            if (year == 2) System.out.println("Your Chinese year is tiger ");
+            if (year == 3) System.out.println("Your Chinese year is rabbit ");
+            if (year == 4) System.out.println("Your Chinese year is dragon ");
+            if (year == 5) System.out.println("Your Chinese year is snake ");
+            if (year == 6) System.out.println("Your Chinese year is horse ");
+            if (year == 7) System.out.println("Your Chinese year is goat ");
+            if (year == 8) System.out.println("Your Chinese year is monkey ");
+            if (year == 9) System.out.println("Your Chinese year is rooster ");
+            if (year == 10) System.out.println("Your Chinese year is dog ");
+            if (year == 11) System.out.println("Your Chinese year is pig ");
+        }
+        else{
+            System.out.println("Your Chinese year is rat ");
+        }
+         */
+        
+        System.out.println("Write a year)");
+        Scanner scanner=new Scanner(System.in);
+        int year=scanner.nextInt();
+        if(year%4==0)
+            System.out.println("In this year 365 days");
+        else System.out.println("In this year 366 days");
+
+
+
+
+
+
+
+        }
+    }
+
+
