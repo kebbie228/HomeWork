3c76afe208dae9f8cee623a908ce6338e05a1c5e
kebbie228 <anton.konovalchenko@gmail.com>
2022-11-09 20:28:44 +0300

    my fifth homework

--- src/homework8_10/TrainList.java
+++ src/homework8_10/TrainList.java
@@ -1,8 +1,5 @@
 package homework8_10;
 
-import homework8_2.Customers;
-import homework8_2.CustomersList;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +10,25 @@ public class TrainList {
         System.out.println("List of train:");
         for(Train train:TrainList)
             System.out.println(train.toString());
+    }
+    public void trainDestination(String destination){
+        System.out.println("List of trains moving towards their destination:"+destination);
+        for(Train train:TrainList)
+            if(train.getDestination().equals(destination))
+            System.out.println(train.toString());
+    }
+    public void trainTimeDestination( String destination,String departureTime) {
+        System.out.println("List of trains moving towards their destination:" + destination + " after " + departureTime);
+        for (Train train : TrainList){
+            if (train.getDestination().equals(destination) && IntegerTime(train.getDepartureTime().substring(0, 2)) >= IntegerTime(departureTime))
+                System.out.println(train.toString());
+            else if (train.getDestination().equals(destination) && IntegerTime(train.getDepartureTime().substring(0, 2)) > 0 && IntegerTime(train.getDepartureTime().substring(0, 2)) <= IntegerTime(departureTime))
+                System.out.println(train.toString());
+    }
+
+    }
+    public int IntegerTime(String departureTime){
+         return Integer.parseInt(departureTime.substring(0,2));
     }
 
 }
--- src/homework8_10/train_10.java
+++ src/homework8_10/train_10.java
@@ -2,12 +2,15 @@ package homework8_10;
 
 public class train_10 {
     public static void main(String[] args) {
-    Train train1 = new Train("Vitebsk", 305, "13:13", 212);
-    Train train2 = new Train("Minsk", 503, "02:43", 373);
-    Train train3 = new Train("Moscow", 777, "23:51", 622);
-    Train train4 = new Train("Orsha", 13, "06:01", 100);
+    Train train1 = new Train("Vitebsk", 305, "13:51", 212);
+    Train train2 = new Train("Minsk", 503, "02:13", 373);
+    Train train3 = new Train("Moscow", 777, "23:23", 622);
+    Train train4 = new Train("Orsha", 13, "06:41", 100);
     Train train5 = new Train("Saint-Petersburg", 101, "10:10", 605);
-    Train train6 = new Train("Brest", 185, "07:10", 137);
+    Train train6 = new Train("Brest", 185, "07:37", 137);
+    Train train7 = new Train("Brest", 394, "09:54", 137);
+    Train train8 = new Train("Minsk", 831, "08:32", 373);
+    Train train9 = new Train("Minsk", 831, "23:32", 373);
     TrainList trainList = new TrainList();
     trainList.addTrain(train1);
     trainList.addTrain(train2);
@@ -15,7 +18,14 @@ public class train_10 {
     trainList.addTrain(train4);
     trainList.addTrain(train5);
     trainList.addTrain(train6);
-    trainList.trainPrintf();
+    trainList.addTrain(train7);
+    trainList.addTrain(train8);
+    trainList.addTrain(train9);
+
+    //trainList.trainPrintf();
+   //trainList.trainDestination("Minsk"); //trains whose move to destination
+  // trainList.trainTimeDestination("Brest","03:00");// trains moving towards their destination after time
+
     }
 
 }
